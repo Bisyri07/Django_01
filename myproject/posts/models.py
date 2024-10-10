566b883ff173374e78f3542af41c26b3d298b09a
bisyri 08 <mbisyri33@gmail.com>
2024-10-10 13:05:44 +0700

    Fix Post class

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=75)
     body = models.TextField()
     slug = models.SlugField()
