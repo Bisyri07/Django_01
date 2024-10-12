3c0d8604b70758cb176c8cc0fb19267ce4318c69
bisyri 08 <mbisyri33@gmail.com>
2024-10-12 17:02:29 +0700

    Python Django Pages, URLs & Slugs

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.posts_list),
+    path(route='', view=views.posts_list, name='list'),
+    path(route='<slug:slug>', view=views.post_page, name='page'),
 ]
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+# from django.http import HttpResponse
 
 # Create your views here.
 def posts_list(request):
@@ -7,3 +8,10 @@ def posts_list(request):
     return render(request=request, 
                   template_name='posts/posts_list.html',
                   context={'posts':posts})
+
+def post_page(request, slug):
+    # return HttpResponse(slug)
+    post = Post.objects.get(slug=slug)
+    return render(request=request, 
+                  template_name='posts/post_page.html',
+                  context={'post':post})
