dfbef59ad8f8e88a5a2ffa05acc4f5d4086be3aa
bisyri 08 <mbisyri33@gmail.com>
2024-10-12 11:19:14 +0700

    Python Django Admin tutorial

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def posts_list(request):
-    return render(request=request, template_name='posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date') # descending
+    return render(request=request, 
+                  template_name='posts/posts_list.html',
+                  context={'posts':posts})
