6987de4dca915d86ee209b6872fc896ced07f907
bisyri 08 <mbisyri33@gmail.com>
2024-10-14 09:48:08 +0700

    Python Django Challenge for Beginners with Solution

---
+++ myproject/users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register_view(request):
+    return render(
+        request=request,
+        template_name='users/register.html',
+    )
+
